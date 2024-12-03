347ae862914b52356f7baea3fae51a0c6039263e
Sebastián González <sebagonz91@gmail.com>
2024-12-02 21:23:31 -0300

    add host to prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+  config.hosts << "blog.sgm.codes"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
